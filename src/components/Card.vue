fab689886ba27916e9f7e65ec6cd5d218c974d84
Ann-Fox <fx8565@mail.ru>
2025-03-27 19:55:48 +0500

    refactor: create card component with value prop

---
+++ src/components/Card.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="card">{{ value }}</div>
+</template>
+
+<script setup>
+defineProps({
+    value: {
+        type: Number,
+        required: true
+    },
+});
+
+</script>
+
+<style>
+.card {
+    border: 5px solid #ccc;
+}
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,27 +1,19 @@
 <template>
   <h1>This is an game</h1>
   <section class="game-board">
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-
+  <Card v-for="(card, index) in cardList" :key="`cadr-${index}`" :value="card"></Card>
   </section>
 </template>
+
+<script setup>
+import Card from "../components/Card.vue"
+
+const cardList = [];
+
+for (let index = 0; index < 18; index++) {
+  cardList.push(index)
+}
+</script>
 
 <style>
 .card {
