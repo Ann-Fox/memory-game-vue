1da66d5a444c8cd0fa06dc507abe8eef8923157a
Ann-Fox <fx8565@mail.ru>
2025-03-27 18:10:47 +0500

    feat: remove unnecessary items

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,22 @@ import TheWelcome from '../components/TheWelcome.vue'
 
 <template>
   <main>
-    <TheWelcome />
+    <div class="parrent"> 
+      <img src="../assets/image/card-joker.png">
+    </div>
+    <h1>Игра 'Memory'</h1>
+    <RouterLink to="/about">Начать игру!</RouterLink>
   </main>
 </template>
+
+<style lang="scss" scoped>
+.parrent {
+  width: 200px;
+  // background: gray;
+}
+
+.parrent img {
+  width: 100%;
+  // mix-blend-mode: multiply;
+}
+</style>
