43c01db63fda7cc2a029e2e065197e9af22f4742
Ann-Fox <fx8565@mail.ru>
2025-03-27 18:17:02 +0500

    feat: create game board scaffald

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,39 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <h1>This is an game</h1>
+  <section class="game-board">
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+
+  </section>
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+.card {
+  border: 5px solid #ccc;
+}
+
+.game-board {
+  display: grid;
+  grid-template-columns: repeat(6, 50px);
+  grid-template-rows: repeat(3, 50px);
+  grid-column-gap: 20px;
+  grid-row-gap: 20px;
+  justify-content: center;
 }
 </style>
