f7759e61a28d6d6ebe2c383f9b796eb374b016e4
Ann-Fox <fx8565@mail.ru>
2025-04-03 13:21:44 +0500

    feat: card has front/back and can be flepped

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,19 +1,60 @@
 <template>
-    <div class="card">{{ value }}</div>
+    <div class="card" @click="selectCard">
+        <div v-if="visible" class="card-face is-front">{{ value }}</div>
+        <div v-else class="card-face is-back">back</div>
+    </div>
 </template>
 
 <script setup>
-defineProps({
+
+import { defineEmits } from 'vue';
+
+const props = defineProps({
+    position: {
+        type: Number,
+        required: true
+    },
     value: {
         type: Number,
         required: true
     },
+    visible: {
+        type: Boolean,
+        default: true
+    },
 });
 
+const emit = defineEmits();
+
+const selectCard = () => {
+  emit('select-card', {
+    position: props.position
+  });
+};
 </script>
 
 <style>
 .card {
+    position: relative;
     border: 5px solid #ccc;
+}
+
+.card-face {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.card-face.is-front {
+    background: red;
+    color: white;
+}
+
+.card-face.is-back {
+    background: blue;
+    color: white;
 }
 </style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,17 +1,27 @@
 <template>
   <h1>This is an game</h1>
   <section class="game-board">
-  <Card v-for="(card, index) in cardList" :key="`cadr-${index}`" :value="card"></Card>
+    <Card v-for="(card, index) in cardList" :key="`cadr-${index}`" :value="card.value" :visible="card.visible"
+      @select-card="flipCard" :position="card.position"></Card>
   </section>
 </template>
 
 <script setup>
 import Card from "../components/Card.vue"
+import {ref} from 'vue'
 
-const cardList = [];
+const cardList = ref([]);
 
 for (let index = 0; index < 18; index++) {
-  cardList.push(index)
+  cardList.value.push({
+    value: index,
+    visible: false,
+    position: index
+  })
+}
+
+const flipCard = (payload) => {
+  cardList.value[payload.position].visible = true
 }
 </script>
 
