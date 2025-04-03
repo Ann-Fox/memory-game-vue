635473538dd5a844ea29af616e62f082c97a4a8c
Ann-Fox <fx8565@mail.ru>
2025-04-03 21:38:14 +0500

    feat: добавлена функция отслеживания выбора и проверки совпадения карт

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="card" @click="selectCard">
-        <div v-if="visible" class="card-face is-front">{{ value }}</div>
+        <div v-if="visible" class="card-face is-front">{{ value }} - {{ matched }}</div>
         <div v-else class="card-face is-back">back</div>
     </div>
 </template>
@@ -10,6 +10,10 @@
 import { defineEmits } from 'vue';
 
 const props = defineProps({
+    matched: {
+        type: Boolean,
+        default: false
+    },
     position: {
         type: Number,
         required: true
@@ -27,9 +31,10 @@ const props = defineProps({
 const emit = defineEmits();
 
 const selectCard = () => {
-  emit('select-card', {
-    position: props.position
-  });
+    emit('select-card', {
+        position: props.position,
+        faceValue: props.value
+    });
 };
 </script>
 
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,27 +2,65 @@
   <h1>This is an game</h1>
   <section class="game-board">
     <Card v-for="(card, index) in cardList" :key="`cadr-${index}`" :value="card.value" :visible="card.visible"
-      @select-card="flipCard" :position="card.position"></Card>
+      @select-card="flipCard" :position="card.position" :matched="card.matched"></Card>
   </section>
+  <h2>{{ status }}</h2>
 </template>
 
 <script setup>
 import Card from "../components/Card.vue"
-import {ref} from 'vue'
+import { ref, watch } from 'vue'
 
 const cardList = ref([]);
+const userSelection = ref([])
+const status = ref('')
 
 for (let index = 0; index < 18; index++) {
   cardList.value.push({
     value: index,
     visible: false,
-    position: index
+    position: index,
+    matched: false
   })
 }
 
-const flipCard = (payload) => {
+const flipCard = payload => {
   cardList.value[payload.position].visible = true
+
+  if (userSelection.value[0]) {
+    userSelection.value[1] = payload
+  } else {
+    userSelection.value[0] = payload
+  }
+  return userSelection
 }
+console.log(userSelection);
+
+watch(userSelection, currentValue => {
+  console.log(currentValue);
+
+  if (currentValue.length === 2) {
+ const cardOne = currentValue[0];
+ const cardTwo = currentValue[1];
+
+ if (cardOne.faceValue === cardTwo.faceValue) {
+  status.value = "Совпадение"
+
+  cardList.value[cardOne.position].matched = true
+  cardList.value[cardTwo.position].matched = true
+ } else {
+  status.value = "Несовпадение"
+
+  cardList.value[cardOne.position].visible = false
+  cardList.value[cardTwo.position].visible = false
+ }
+    
+
+
+    userSelection.value.length = 0
+  }
+}, { deep: true })
+
 </script>
 
 <style>
