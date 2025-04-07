3eed66771ec804bd48c3a8d547a9390c5157de23
Ann-Fox <fx8565@mail.ru>
2025-04-07 13:54:32 +0500

    fix: пользователь не может нажать на одну и туже карту

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="card" @click="selectCard">
-        <div v-if="visible" class="card-face is-front">{{ value }} - {{ position }}</div>
+        <div v-if="visible" class="card-face is-front">{{ value }} - {{ position }} - {{ matched }}</div>
         <div v-else class="card-face is-back">back</div>
     </div>
 </template>
